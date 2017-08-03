e7104cd89668377bc355bc4cbb636a26b4b17117
Sam Lau <esayemm@gmail.com>
2017-08-03 12:14:42 -0700

    themed form

--- example/demos/themed-form.js
+++ example/demos/themed-form.js
@@ -89,6 +89,77 @@ export default {
     ],
     codeSnippetType: 'jsx',
     codeSnippet    : `
+class FormExample extends React.Component {
+  state = {
+    formState: null,
+  }
+
+  componentWillMount() {
+    const formState = new FormState({
+      validators: {
+        email: (value = '') => {
+          if (value.length <= 0) {
+            throw new Error('Cannot be empty')
+          }
+          return true
+        },
+        password: () => {
+          return true
+        },
+        confirmPassword: (value, allValues) => {
+          if (value !== allValues.password) {
+            throw new Error('Must be the same as password')
+          }
+          return true
+        },
+      },
+    })
+    this.setState({ formState })
+  }
+
+  render() {
+    const {
+      formState,
+    } = this.state
+
+    return <ThemedForm
+      formState={formState}
+      style={{
+        boxShadow : '0 5px 15px #090909',
+        paddingTop: '1rem',
+      }}
+    >
+      <ThemedFormField
+        label={'email'}
+        style={{display: 'block'}}
+        formState={formState}
+        formFieldKey={'email'}
+        placeholder={'email'}
+      />
+      <ThemedFormField
+        label={'Password'}
+        style={{display: 'block'}}
+        formState={formState}
+        formFieldKey={'password'}
+        placeholder={'password'}
+      />
+      <ThemedFormField
+        label={'Confirm Password'}
+        style={{display: 'block'}}
+        formState={formState}
+        formFieldKey={'confirmPassword'}
+        placeholder={'Confirm Password'}
+      />
+
+      <Button
+        style={{ width: '100%', borderRadius: '0', marginTop: '1rem' }}
+        color={variables.green3}
+      >
+      Submit
+      </Button>
+    </ThemedForm>
+  }
+}
     `,
   },
 }
