3c03f5bb6d585d6ddd598f224ef19f429ea9968c
Sam Lau <esayemm@gmail.com>
2017-09-06 16:33:17 -0700

    remove test for code?

--- src/components/Transition/index.js
+++ src/components/Transition/index.js
@@ -2,24 +2,35 @@ import styles               from './styles.scss'
 import PropTypes            from 'prop-types'
 import {CSSTransitionGroup} from 'react-transition-group'
 
+const ALLOWED_TYPES = {
+  fade: {
+    enterTimeout: 500,
+    leaveTimeout: 500,
+  },
+}
+
 @cssModule(styles)
 export default class Transition extends React.Component {
   static propTypes = {
-
+    type: PropTypes.oneOf(Object.keys(ALLOWED_TYPES)),
   }
 
   static defaultProps = {
-
+    type: 'fade',
   }
 
   render() {
     const {
+      type,
       children,
     } = this.props
+
+    const selectedType = ALLOWED_TYPES[type]
+
     return <CSSTransitionGroup
-      transitionName={'tran'}
-      transitionEnterTimeout={500}
-      transitionLeaveTimeout={500}
+      transitionName={`rui-${type}`}
+      transitionEnterTimeout={selectedType.enterTimeout}
+      transitionLeaveTimeout={selectedType.leaveTimeout}
     >
       {children}
     </CSSTransitionGroup>
