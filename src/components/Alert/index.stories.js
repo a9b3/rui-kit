696e6383cad828accbf4c7d710617cb35bfdde6b
Sam Lau <esayemm@gmail.com>
2017-12-05 16:04:55 -0800

    add storybook knobs

--- src/components/Alert/index.stories.js
+++ src/components/Alert/index.stories.js
@@ -1,11 +1,13 @@
 import {storiesOf} from '@storybook/react'
+import { boolean, withKnobs } from '@storybook/addon-knobs'
 
 import Alert from './index.js'
 
 storiesOf('Alert', module)
+  .addDecorator(withKnobs)
   .add('default', () => {
     return <Alert
-      show
+      show={boolean('show', true)}
     >
       hi
     </Alert>
