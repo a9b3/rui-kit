fc559cdd16957dfffb20beaaf7aca3482f9ed7cd
Sam Lau <esayemm@gmail.com>
2018-04-12 01:31:56 -0700

    prettier on storybook config

--- storybook/config.js
+++ storybook/config.js
@@ -1,11 +1,12 @@
-import {configure} from '@storybook/react'
+import { configure } from '@storybook/react'
 
+// load global stylings
 import '../example/styles/index.scss'
 import '../src/variables.css'
 
 function loadStories() {
   const req = require.context('../src', true, /\.stories\.js$/)
-  req.keys().forEach((filename) => req(filename))
+  req.keys().forEach(filename => req(filename))
 }
 
 configure(loadStories, module)
